6b7af2ac1a87e9de6ab93450647205cb8a1226c1
EcoRanger <adiputrachristian17@gmail.com>
2021-05-05 22:29:47 +0700

    Initial Commit

---
+++ NewLatteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewLatteria
+//
+//  Created by Christian Adiputra on 05/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
